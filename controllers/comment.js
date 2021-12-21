f3ec68d66320e12398d876f27f376137f0a80226
Hassene Ayoub <53129325+Hassene66@users.noreply.github.com>
2021-12-21 23:50:18 +0100

    fixing old issues

--- controllers/comment.js
+++ controllers/comment.js
@@ -2,64 +2,79 @@ const Post = require("../models/Post");
 const ErrorResponse = require("../utils/errorResponse");
 const mongoose = require("mongoose");
 const { validationResult } = require("express-validator");
-exports.addComment = async (req, res) => {
+
+exports.addComment = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  } else {
-    if (!req.params.id) {
-      return ErrorResponse("Veuillez fournir l'ID du post ", 400);
-    }
-  }
-  const { text, owner } = req.body;
-
-  const post = await Post.findByIdAndUpdate(
-    { _id: req.params.id },
-    [
-      {
-        $set: {
-          comments: {
-            $concatArrays: [
-              "$comments",
-              [
-                {
-                  _id: mongoose.Types.ObjectId(owner),
-                  owner: mongoose.Types.ObjectId(owner),
-                  text,
-                },
-              ],
-            ],
-          },
-        },
-      },
-    ],
-    { new: true },
-    null
-  );
-  res.status(200).json({ success: true, post: post });
-};
-exports.deleteComment = async (req, res) => {
-  if (!req.body.CommentID) {
-    return ErrorResponse("Veuillez fournir l'ID du commentaire ", 400);
+    return next(res.status(400).json({ errors: errors.array() }));
   }
   if (!req.params.id) {
-    return ErrorResponse("Veuillez fournir l'ID du post ", 400);
+    return next(
+      new ErrorResponse("Veuillez fournir l'ID du publication ", 400)
+    );
   }
-  const post = await Post.findByIdAndUpdate(
-    { _id: req.params.id },
-    { $pull: { comments: { _id: req.body.CommentID } } },
-    { new: true },
-    null
-  );
-  res.status(200).json({ success: true, post: post });
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const { text, owner } = req.body;
+
+    const post = await Post.findByIdAndUpdate(
+      { _id: req.params.id },
+      [
+        {
+          $set: {
+            comments: {
+              $concatArrays: [
+                "$comments",
+                [
+                  {
+                    _id: mongoose.Types.ObjectId(owner),
+                    owner: mongoose.Types.ObjectId(owner),
+                    text,
+                  },
+                ],
+              ],
+            },
+          },
+        },
+      ],
+      { new: true },
+      null
+    );
+    if (!post) {
+      return next(
+        new ErrorResponse("Publication non trouvé avec l’id fourni", 404)
+      );
+    }
+    res.status(200).json({ success: true, post: post });
+  } else {
+    return next(new ErrorResponse("Veuillez fournir une ID valide", 500));
+  }
 };
-// exports.getComment = async (req, res) => {
-//   const comment = await Comment.findById({ _id: req.params.id }).populate({
-//     path: "postID",
-//     populate: {
-//       path: "likes",
-//       model: "User",
-//     },
-//   });
-//   res.status(200).json({ success: true, res: comment });
-// };
+
+exports.deleteComment = async (req, res, next) => {
+  if (!req.body.CommentID) {
+    return next(
+      new ErrorResponse("Veuillez fournir l'ID du commentaire ", 400)
+    );
+  }
+  if (!req.params.id) {
+    return next(
+      new ErrorResponse("Veuillez fournir l'ID du publication ", 400)
+    );
+  }
+  if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+    const post = await Post.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $pull: { comments: { _id: req.body.CommentID } } },
+      { new: true },
+      null
+    );
+    if (!post) {
+      return next(
+        new ErrorResponse("Publication non trouvé avec l’id fourni", 404)
+      );
+    }
+    res.status(200).json({ success: true, post: post });
+  } else {
+    return next(new ErrorResponse("Veuillez fournir une ID valide", 500));
+  }
+};
